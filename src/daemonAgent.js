ffde9bec2f93087f75e550edd5c0042ace4df28a
Fabrizio Mirabito <f.mirabito@arduino.cc>
2018-06-14 12:09:43 +0200

    added daemonAgent module with all working functionalities, license and config files

---
+++ src/daemonAgent.js
@@ -0,0 +1,311 @@
+/*
+ * This file is part of create-plugin-communication.
+ *
+ * Copyright 2018 Arduino AG (http://www.arduino.cc/)
+ *
+ * create-plugin-communication is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * As a special exception, you may use this file as part of a free software
+ * library without restriction.  Specifically, if other files instantiate
+ * templates or use macros or inline functions from this file, or you compile
+ * this file and link it with other files to produce an executable, this
+ * file does not by itself cause the resulting executable to be covered by
+ * the GNU General Public License.  This exception does not however
+ * invalidate any other reasons why the executable file might be covered by
+ * the GNU General Public License.
+ */
+
+import io from 'socket.io-client';
+import { parseMessage, sendMessage, addPortsCallback, addSerialCallback, initSocket, initPluginUrl, upload, stopUpload } from './readMessages';
+
+// Required agent version
+const MIN_VERSION = '1.1.69';
+
+const PROTOCOL = {
+  HTTP: 'http',
+  HTTPS: 'https'
+};
+
+const LOOPBACK_ADDRESS = '127.0.0.1';
+const LOOPBACK_HOSTNAME = 'localhost';
+const LOOKUP_PORT_START = 8991;
+const LOOKUP_PORT_END = 9000;
+
+let selectedProtocol = PROTOCOL.HTTP;
+let agentInfo = {};
+let found = false;
+const tryInterval = 2500;
+let pollingId = null;
+let socket = null;
+let portsPolling = null;
+
+let wsConnectCb = null;
+let wsErrorCb = null;
+let wsDisconnectCb = null;
+
+const errorMessage = 'Arduino Create Agent cannot be found';
+
+// Stolen from https://github.com/substack/semver-compare
+const semVerCompare = (a, b) => {
+  const pa = a.split('.');
+  const pb = b.split('.');
+  for (let i = 0; i < 3; i += 1) {
+    const na = Number(pa[i]);
+    const nb = Number(pb[i]);
+    if (na > nb) {
+      return 1;
+    }
+    if (nb > na) {
+      return -1;
+    }
+    if (!Number.isNaN(na) && Number.isNaN(nb)) {
+      return 1;
+    }
+    if (Number.isNaN(na) && !Number.isNaN(nb)) {
+      return -1;
+    }
+  }
+  return 0;
+};
+
+/**
+ * Check the agent version and call the update if needed.
+ */
+const update = () => new Promise(resolve => {
+  if (agentInfo.version && (semVerCompare(agentInfo.version, MIN_VERSION) >= 0 || agentInfo.version.indexOf('dev') !== -1)) {
+    resolve(agentInfo);
+  }
+
+  return fetch(`${agentInfo[selectedProtocol]}/update`, {
+    method: 'POST'
+  });
+});
+
+/**
+ * Uses the websocket protocol to connect to the agent
+ *
+ * @return {Promise}
+ */
+const connect = () => {
+  if (socket) {
+    return;
+  }
+
+  let wsProtocol = 'ws';
+
+  if (selectedProtocol === PROTOCOL.HTTPS) {
+    wsProtocol = 'wss';
+  }
+
+  const address = agentInfo[wsProtocol];
+  socket = io(address);
+
+  socket.on('connect', () => {
+
+    initSocket(socket);
+
+    // On connect download windows drivers which are indispensable for detection of boards
+    socket.emit('command', 'downloadtool windows-drivers latest arduino keep');
+    socket.emit('command', 'downloadtool bossac 1.7.0 arduino keep');
+
+    if (typeof wsConnectCb === 'function') {
+      wsConnectCb();
+    }
+
+    // Periodically asks for the ports
+    if (!portsPolling) {
+      portsPolling = setInterval(() => {
+        socket.emit('command', 'list');
+      }, 1500);
+    }
+  });
+
+  socket.on('error', () => {
+    if (typeof wsErrorCb === 'function') {
+      wsErrorCb();
+    }
+  });
+
+  // Reconnect on disconnect
+  socket.on('disconnect', () => {
+    clearInterval(portsPolling);
+    portsPolling = null;
+    if (typeof wsDisconnectCb === 'function') {
+      wsDisconnectCb();
+    }
+    connect();
+  });
+
+  // Parse messages
+  socket.on('message', parseMessage);
+};
+
+/**
+ * Try ports for the selected hostname. From LOOKUP_PORT_START to LOOKUP_PORT_END
+ * @param {string} hostname - The hostname value (LOOPBACK_ADDRESS or LOOPBACK_HOSTNAME).
+ * @return {object} info - The agent info values.
+ */
+const tryPorts = hostname => {
+  const pluginLookups = [];
+
+  for (let port = LOOKUP_PORT_START; port < LOOKUP_PORT_END; port += 1) {
+    pluginLookups.push(fetch(`${selectedProtocol}://${hostname}:${port}/info`).then(response => {
+      return response.json().then(data => {
+        return {
+          response,
+          data
+        };
+      });
+    })
+      .catch(() => {
+        // We expect most of those call to fail, because there's only one agent
+        // So we have to resolve them with a false value to let the Promise.all catch all the deferred data
+        Promise.resolve(false);
+      }));
+  }
+
+  return Promise.all(pluginLookups).then(responses => {
+    found = responses.some(r => {
+      if (r && r.response && r.response.status === 200) {
+        agentInfo = r.data;
+        connect();
+        if (r.response.url.indexOf(PROTOCOL.HTTPS) === 0) {
+          selectedProtocol = PROTOCOL.HTTPS;
+        }
+        initPluginUrl(agentInfo[selectedProtocol]);
+        return true;
+      }
+      return false;
+    });
+
+    if (found) {
+      return update()
+        .then(() => agentInfo);
+    }
+    return Promise.reject(new Error(`${errorMessage} at ${hostname}`));
+  });
+};
+
+const DaemonAgent = (callbacks) => {
+
+  wsConnectCb = callbacks.onSocketConnect;
+  wsErrorCb = callbacks.onSocketError;
+  wsDisconnectCb = callbacks.onSocketDisconnect;
+
+  addPortsCallback(callbacks.onPortsUpdate);
+  addSerialCallback(callbacks.onSerialOutput);
+
+  /**
+   * Set onPortUpdate callback.
+   */
+  const onPortsUpdate = (onPortsUpdateCb) => {
+    callbacks.onPortsUpdate = onPortsUpdateCb;
+    addPortsCallback(callbacks.onPortsUpdate);
+  };
+
+  /**
+   * Set onSerialOutput callback.
+   */
+  const onSerialOutput = (onSerialOutputCb) => {
+    callbacks.onSerialOutput = onSerialOutputCb;
+    addSerialCallback(callbacks.onSerialOutput);
+  };
+
+  /**
+   * Set onSocketConnect callback.
+   */
+  const onSocketConnect = (onSocketConnectCb) => {
+    wsConnectCb = onSocketConnectCb;
+  };
+
+  /**
+   * Set onErrorCb callback.
+   */
+  const onError = (onErrorCb) => {
+    wsErrorCb = onErrorCb;
+  };
+
+  /**
+   * Set onDisconnect callback.
+   */
+  const onDisconnect = (onDisconnectCb) => {
+    wsDisconnectCb = onDisconnectCb;
+  };
+
+  /**
+   * Check if socket connected.
+   * @return {boolean} The connection status flag.
+   */
+  const connected = () => socket && socket.connected;
+
+  /**
+   * Look for the agent endpoint.
+   * First search in http://LOOPBACK_ADDRESS, after in https://LOOPBACK_HOSTNAME.
+   * @return {object} The found agent info values.
+   */
+  const findService = () => {
+    if (found) {
+      return fetch(agentInfo[selectedProtocol])
+        .then(response => response.json())
+        .catch(() => {
+          found = false;
+          return Promise.reject(new Error(errorMessage));
+        });
+    }
+
+    return tryPorts(LOOPBACK_ADDRESS).catch(() => tryPorts(LOOPBACK_HOSTNAME)
+      .catch(() => Promise.reject(new Error(errorMessage))));
+  };
+
+  /**
+   * Perform a find() every interval ms
+   * @return {object} The found agent info values.
+   */
+  const waitForPlugin = (interval = tryInterval) => {
+    pollingId = setInterval(() => {
+      this.findService().then(() => {
+        clearInterval(pollingId);
+      });
+    }, interval);
+  };
+
+  /**
+   * Pauses the plugin
+   * @return {Promise}
+   */
+  const stopPlugin = () => {
+    if (found) {
+      return fetch(`${agentInfo[selectedProtocol]}/pause`);
+    }
+  };
+
+  return {
+    connect,
+    connected,
+    findService,
+    waitForPlugin,
+    stopPlugin,
+    upload,
+    stopUpload,
+    sendMessage,
+    onDisconnect,
+    onSocketConnect,
+    onPortsUpdate,
+    onSerialOutput,
+    onError
+  };
+};
+
+export default DaemonAgent;
---
+++ src/readMessages.js
@@ -0,0 +1,376 @@
+/*
+ * This file is part of create-plugin-communication.
+ *
+ * Copyright 2018 Arduino AG (http://www.arduino.cc/)
+ *
+ * create-plugin-communication is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * As a special exception, you may use this file as part of a free software
+ * library without restriction.  Specifically, if other files instantiate
+ * templates or use macros or inline functions from this file, or you compile
+ * this file and link it with other files to produce an executable, this
+ * file does not by itself cause the resulting executable to be covered by
+ * the GNU General Public License.  This exception does not however
+ * invalidate any other reasons why the executable file might be covered by
+ * the GNU General Public License.
+ */
+
+const callbacks = {
+  ports: [],
+  program: [],
+  serial: [],
+  download: []
+};
+const promises = {};
+let uploading = false;
+
+let networkPorts = [];
+let serialPorts = [];
+
+let socket = null;
+let pluginURL = null;
+
+// Sends the data to all the registered callbacks
+const callback = (name, data) => {
+  if (!callbacks || !callbacks[name] || !callbacks[name].length) {
+    return;
+  }
+  for (const cb of callbacks[name]) {
+    cb(data);
+  }
+};
+
+const parseMessageList = (data) => {
+  if (data.Network) {
+    networkPorts = data.Ports;
+  } else {
+    serialPorts = data.Ports;
+  }
+  callback('ports', serialPorts.concat(networkPorts));
+};
+
+const parseMessageCommand = (data) => {
+  if (data.Cmd === 'Open') {
+    if (promises.open) {
+      promises.open.resolve(true);
+    }
+  } else if (data.Cmd === 'OpenFail') {
+    if (promises.open) {
+      promises.open.reject(data.Desc);
+    }
+  } else if (data.Cmd === 'Close') {
+    if (promises.close) {
+      promises.close.resolve(true);
+      promises.close = null;
+    }
+  } else if (data.Cmd === 'CloseFail') {
+    if (promises.close) {
+      promises.close.reject(data.Desc);
+      promises.close = null;
+    }
+  }
+};
+
+const parseFlash = (data) => {
+  if (!uploading) {
+    return;
+  }
+  uploading = false;
+  callback('program', {done: true, msg: data.Flash + '\n'});
+};
+
+const parseProgram = (data) => {
+  if (!uploading) {
+    return;
+  }
+  if (data.ProgrammerStatus === 'Starting') {
+    callback('program', {done: false, msg: 'Programming with: ' + data.Cmd + '\n'});
+  } else if (data.ProgrammerStatus === 'Busy') {
+    callback('program', {done: false, msg: data.Msg + '\n'});
+  } else if (data.ProgrammerStatus === 'Error') {
+    uploading = false;
+    callback('program', {done: true, err: data.Msg + '\n'});
+  } else if (data.ProgrammerStatus === 'Killed') {
+    uploading = false;
+    callback('program', {done: false, msg: 'terminated by user\n'});
+    callback('program', {done: true, msg: 'terminated by user\n'});
+  } else if (data.ProgrammerStatus === 'Error 404 Not Found') {
+    uploading = false;
+    callback('program', {done: true, err: data.Msg + '\n'});
+  }
+};
+
+const parseDownload = (data) => {
+  if (data.DownloadStatus === 'Pending') {
+    callback('download', data.Msg);
+  } else if (data.DownloadStatus === 'Success') {
+    if (promises.download) {
+      promises.download.resolve(data.Msg);
+    }
+  } else if (promises.download) {
+    promises.download.reject(data.Msg);
+  }
+};
+
+const parseMessage = message => {
+  let jsonMessage;
+
+  try {
+    jsonMessage = JSON.parse(message);
+  } catch (SyntaxError) {
+    return;
+  }
+
+  // Result of a list command
+  if (jsonMessage && jsonMessage.Ports) {
+    return parseMessageList(jsonMessage);
+  }
+
+  // Result of a Open or Close command
+  if (jsonMessage && jsonMessage.Cmd) {
+    return parseMessageCommand(jsonMessage);
+  }
+
+  if (jsonMessage && jsonMessage.Error) {
+    if (jsonMessage.Error.indexOf('could not find') !== -1) {
+      if (promises.close) {
+        promises.close.reject(jsonMessage.Error);
+        promises.close = null;
+      }
+    }
+    return;
+  }
+
+  // Result of a Program command
+  if (jsonMessage && jsonMessage.Flash) {
+    return parseFlash(jsonMessage);
+  }
+
+  if (jsonMessage.Err) {
+    return callback('program', {done: false, err: jsonMessage.Err + '\n'});
+  }
+
+  if (jsonMessage && jsonMessage.ProgrammerStatus) {
+    return parseProgram(jsonMessage);
+  }
+
+  // Result of a download command
+  if (jsonMessage && jsonMessage.DownloadStatus) {
+    return parseDownload(jsonMessage);
+  }
+
+  // Data read from the serial
+  if (jsonMessage.D) {
+    return callback('serial', {data: jsonMessage.D});
+  }
+};
+
+function Deferred() {
+	// update 062115 for typeof
+	if (typeof(Promise) != 'undefined' && Promise.defer) {
+		//need import of Promise.jsm for example: Cu.import('resource:/gree/modules/Promise.jsm');
+		return Promise.defer();
+	} else if (typeof(PromiseUtils) != 'undefined'  && PromiseUtils.defer) {
+		//need import of PromiseUtils.jsm for example: Cu.import('resource:/gree/modules/PromiseUtils.jsm');
+		return PromiseUtils.defer();
+	} else {
+		/* A method to resolve the associated Promise with the value passed.
+		 * If the promise is already settled it does nothing.
+		 *
+		 * @param {anything} value : This value is used to resolve the promise
+		 * If the value is a Promise then the associated promise assumes the state
+		 * of Promise passed as value.
+		 */
+		this.resolve = null;
+
+		/* A method to reject the assocaited Promise with the value passed.
+		 * If the promise is already settled it does nothing.
+		 *
+		 * @param {anything} reason: The reason for the rejection of the Promise.
+		 * Generally its an Error object. If however a Promise is passed, then the Promise
+		 * itself will be the reason for rejection no matter the state of the Promise.
+		 */
+		this.reject = null;
+
+		/* A newly created Promise object.
+		 * Initially in pending state.
+		 */
+		this.promise = new Promise(function (resolve, reject){
+			this.resolve = resolve;
+			this.reject = reject;
+		}.bind(this));
+	}
+}
+
+const sendMessage = (action, data, cb) => {
+
+  let deferred = new Deferred();
+  const replacementStrategy = 'keep';
+
+  if (!socket) {
+    deferred.reject();
+    return deferred.promise;
+  }
+
+  if (uploading && action !== 'req_downloadtool') {
+    deferred.reject(false);
+    return deferred.promise;
+  }
+
+  if (action === 'req_serial_monitor_open') {
+    socket.emit('command', 'open ' + data.com_name + ' ' + data.baudrate + ' timed', function () {
+      promises.open = deferred;
+    });
+  } else if (action === 'req_serial_monitor_write') {
+    socket.emit('command', 'send ' + data.com_name + ' ' + data.data, () => {
+      deferred.resolve(true);
+    });
+  } else if (action === 'req_serial_monitor_close') {
+    socket.emit('command', 'close ' + data.com_name, () => {
+      if (promises.close) {
+        return promises.close;
+      }
+      promises.close = deferred;
+    });
+  } else if (action === 'req_downloadtool') {
+    if (cb) {
+      callbacks.download = [cb];
+    }
+    if (data.tool) {
+      socket.emit('command', 'downloadtool ' + data.tool + ' ' + data.tool_version + ' ' + data.package + ' ' + replacementStrategy, () => {
+        promises.download = deferred;
+      });
+    } else {
+      deferred.resolve('no need to download a nonexistent tool');
+      promises.download = null;
+    }
+  }
+  return deferred.promise;
+};
+
+const initSocket = (socketInstance) => {
+  socket = socketInstance;
+};
+
+const addPortsCallback = (portsCb) => {
+  if (typeof portsCb === 'function') {
+    callbacks.ports.push(portsCb);
+  }
+};
+
+const addSerialCallback = (serialCb) => {
+  if (typeof serialCb === 'function') {
+    callbacks.serial.push(serialCb);
+  }
+};
+
+const initPluginUrl = (selectedPluginUrl) => {
+  pluginURL = selectedPluginUrl;
+}
+
+/**
+ * Perform an upload via http on the daemon
+ * file = {name: 'filename', data: 'base64data'}
+ * target = {
+ *       board: "name of the board",
+ *       port: "port of the board",
+ *       auth_user: "Optional user to use as authentication",
+ *       auth_pass: "Optional pass to use as authentication"
+ *       auth_key: "Optional private key",
+ *       auth_port: "Optional alternative port (default 22)"
+ *       network: true or false
+ *    }
+ *    data = {
+ *       commandline: "commandline to execute",
+ *       signature: "signature of the commandline",
+ *       files: [
+ *          {name: "Name of a file to upload on the device", data: 'base64data'}
+ *       ],
+ *       options: {}
+ *    }
+ *    cb = callback function executing everytime a packet of data arrives through the websocket
+ */
+const upload = (target, data, cb) => {
+  uploading = true;
+  callbacks.program = [cb];
+
+  // At least one file to upload
+  if (data.files.length === 0) {
+    uploading = false;
+    callback('program', {done: true, err: 'You need at least one file to upload'});
+    return;
+  }
+
+  // Main file
+  const file = data.files[0];
+  file.name = file.name.split('/');
+  file.name = file.name[file.name.length - 1];
+
+  const payload = {
+    board: target.board,
+    port: target.port,
+    commandline: data.commandline,
+    signature: data.signature,
+    hex: file.data,
+    filename: file.name,
+    extra: {
+      auth: {
+        username: target.auth_user,
+        password: target.auth_pass,
+        private_key: target.auth_key,
+        port: target.auth_port
+      },
+      wait_for_upload_port: data.options.wait_for_upload_port === 'true' || data.options.wait_for_upload_port === true,
+      use_1200bps_touch: data.options.use_1200bps_touch === 'true' || data.options.use_1200bps_touch === true,
+      network: target.network,
+      ssh: target.ssh,
+      params_verbose: data.options.param_verbose,
+      params_quiet: data.options.param_quiet,
+      verbose: data.options.verbose
+    },
+    extrafiles: data.extrafiles || []
+  };
+
+  for (let i = 1; i < data.files.length; i++) {
+    payload.extrafiles.push({filename: data.files[i].name, hex: data.files[i].data});
+  }
+
+  return fetch(`${pluginURL}/upload`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'text/plain; charset=utf-8'
+    },
+    body: JSON.stringify(payload)
+  })
+    .catch(error => {
+      callback('program', {done: true, err: error});
+    });
+};
+
+const stopUpload = () => {
+  uploading = false;
+  socket.emit('command', 'killprogrammer');
+};
+
+export {
+  parseMessage,
+  sendMessage,
+  addPortsCallback,
+  addSerialCallback,
+  initSocket,
+  initPluginUrl,
+  upload,
+  stopUpload
+};
