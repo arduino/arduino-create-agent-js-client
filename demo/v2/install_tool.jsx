1a6b3fc747545bfe0e1f7e6f07bb61a68fe6d04c
Matteo Suppo <matteo.suppo@gmail.com>
2019-03-29 12:25:52 +0100

    Add default testing values for v2.upload

--- demo/v2/install_tool.jsx
+++ demo/v2/install_tool.jsx
@@ -5,12 +5,12 @@ export class V2InstallTool extends React.Component {
   constructor() {
     super();
     this.state = {
-      name: "",
-      version: "",
-      packager: "",
-      url: "",
-      checksum: "",
-      signature: "",
+      name: "avrdude",
+      version: "6.3.0-arduino9",
+      packager: "arduino",
+      url: "http://downloads.arduino.cc/tools/avrdude-6.3.0-arduino9-i686-w64-mingw32.zip",
+      checksum: "SHA-256:f3c5cfa8d0b3b0caee81c5b35fb6acff89c342ef609bf4266734c6266a256d4f",
+      signature: "7628b488c7ffd21ae1ca657245751a4043c419fbab5c256a020fb53f17eb88686439f54f18e78a80b40fc2de742f79b78ed4338c959216dc8ae8279e482d2d4117eeaf34a281ce2369d1dc4356f782c0940d82610f1c892e913b637391c39e95d4d4dfe82d8dbc5350b833186a70a62c7952917481bad798a9c8b4905df91bd914fbdfd6e98ef75c8f7fb06284278da449ce05b27741d6eda156bbdb906d519ff7d7d5042379fdfc55962b3777fb9240b368552182758c297e39c72943d75d177f2dbb584b2210301250796dbe8af11f0cf06d762fe4f912294f4cdc8aff26715354cfb33010a81342fbbc438912eb424a39fc0c52a9b2bf722051a6f3b024bd",
       res: ""
     };
 
