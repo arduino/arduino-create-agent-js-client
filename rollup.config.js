6ddf5719fb1f34978eff7d46516cc94f493da99e
pirropirro <fra.pirrotta@gmail.com>
2019-11-13 12:37:15 +0100

    Update rollup configurations

--- rollup.config.js
+++ rollup.config.js
@@ -5,13 +5,15 @@ import replace from 'rollup-plugin-replace';
 import uglify from 'rollup-plugin-uglify-es';
 
 const env = process.env.NODE_ENV;
-const config = {
-  format: 'umd',
-  moduleName: 'CreatePlugin',
+const isProduction = env === 'production';
+
+export default {
+  output: {
+    format: 'umd',
+    name: 'CreatePlugin',
+  },
   plugins: [
-    nodeResolve({
-      jsnext: true
-    }),
+    nodeResolve(),
     // due to https://github.com/rollup/rollup/wiki/Troubleshooting#name-is-not-exported-by-module
     commonjs({
       include: 'node_modules/**',
@@ -22,19 +24,14 @@ const config = {
     }),
     replace({
       'process.env.NODE_ENV': JSON.stringify(env)
+    }),
+    isProduction && uglify({
+      compress: {
+        pure_getters: true,
+        unsafe: true,
+        unsafe_comps: true,
+        warnings: false
+      }
     })
   ]
 };
-
-if (env === 'production') {
-  config.plugins.push(uglify({
-    compress: {
-      pure_getters: true,
-      unsafe: true,
-      unsafe_comps: true,
-      warnings: false
-    }
-  }));
-}
-
-export default config;
