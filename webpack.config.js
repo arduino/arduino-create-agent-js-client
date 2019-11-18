10b31f0218cea7eb6740415ccd4766758c1134ef
pirropirro <fra.pirrotta@gmail.com>
2019-11-18 09:58:45 +0100

    Update rollup from 0.68.2 to 1.27.0 (#249)
    * Bump babel & rollup versions
    
    * update babel configurations
    
    * Update rollup configurations

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,10 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /(node_modules)/,
         use: {
-          loader: 'babel-loader'
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
         }
       }
     ]
