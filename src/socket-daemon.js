6dd8533d17b4e38c182ca3ee04c18090192c2d89
Umberto Baldi <34278123+umbynos@users.noreply.github.com>
2021-02-03 09:16:20 +0100

    update agent autoupdate behaviour (#439)
    * update agent autoupdate behaviour
    
    - Now it's possible to rollback the agent  by modifying the `agent-version.json` on s3
    - Update mechanism is not triggered anymore if the agent contains a semver with `rc` (e.g. 1.2.1-rc1)
    
    * Update socket-daemon.js
    
    fixed comparison
    
    Co-authored-by: Stefania <s.mellai@arduino.cc>

--- src/socket-daemon.js
+++ src/socket-daemon.js
@@ -165,7 +165,7 @@ export default class SocketDaemon extends Daemon {
         if (found) {
           return fetch('https://s3.amazonaws.com/arduino-create-static/agent-metadata/agent-version.json')
             .then(response => response.json().then(data => {
-              if (this.agentInfo.version && (semVerCompare(this.agentInfo.version, data.Version) >= 0 || this.agentInfo.version.indexOf('dev') !== -1)) {
+              if (this.agentInfo.version && (semVerCompare(this.agentInfo.version, data.Version) === 0 || this.agentInfo.version.indexOf('dev') !== -1 || this.agentInfo.version.indexOf('rc') !== -1)) {
                 return this.agentInfo;
               }
 
